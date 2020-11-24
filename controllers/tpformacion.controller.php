a3ba8661f070e520ae0c3db6734bd32d3ce3af19
julio cesar ruiz <juliocruizt@gmail.com>
2020-11-24 17:52:14 -0500

    Dia 24 de noviembre del 2020

---
+++ controllers/tpformacion.controller.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'models/tpformacion.php';
+
+class TpformacionController {
+
+	private $tpformacion;
+
+	public function __Construct() {
+		$this->tpformacion = new Tpformacion(); // Instancia de la Clase del Modelo Usuario
+	}
+
+	public function IndexSelect() {
+		require_once 'views/tpformacion/tpformacionSelect.php';
+	}
+
+	public function Index() {
+		require_once 'views/tpformacion/index.php';
+	}
+
+	public function Eliminar() {
+		$this->tpformacion->Delete($_REQUEST['id']);
+		require_once 'views/tpformacion/tpformacionSelect.php';
+	}
+
+	public function Insertar() {
+
+		$datos = $this->tpformacion;
+
+		$datos->name = $_REQUEST['nombre'];
+
+		$this->tpformacion->Insert($datos);
+
+		require_once 'views/tpformacion/tpformacionSelect.php';
+	}
+	public function Actualizar() {
+
+		$datos = $this->pformacion;
+
+		$datos->name = $_REQUEST['nombre'];
+		$datos->id = $_REQUEST['id'];
+		$this->tpformacion->Update($datos);
+
+		require_once 'views/tpformacion/tpformacionSelect.php';
+	}
+
+}
+?>
--- views/tpformacion/index.php
+++ views/tpformacion/index.php
@@ -1,18 +1,19 @@
 <?php include_once 'views/components/head.php';?>
 <?php include_once 'views/components/header.php';?>
 
-<script src="views/assets/js/scripts.jornada.js"></script>
+<script src="views/assets/js/scripts.pformacion.js"></script>
 <div id="layoutSidenav">
     <?php include_once 'views/components/sidebar.php';?>
     <div id="layoutSidenav_content">
         <main>
             <div class="container-fluid">
                 <div id="tview">
-                    <?php include 'jornadaSelect.php';?>
+                    <h3 class="mt-4 mb-4">Gestionar Fases</h3>
+                    <?php include 'tpformacionSelect.php';?>
                 </div>
                 <button data-toggle="modal" title="Crear Jornada" class="open-modal btn btn-dark rounded-circle" data-target="#modal">+</button>
 
-            	<?php include 'jornadaInsert.php';?>
+            	<?php include 'tpformacionInsert.php';?>
             </div>
         </main>
     </div>
